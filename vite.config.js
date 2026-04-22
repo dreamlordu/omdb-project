c845ce04bbb268db8c743935f24e52cd9bc0c4f3
Osman Gözütok <osmangztk16@gmail.com>
2026-04-22 21:06:31 +0300

    Clean: Dependencies restored and ready for Vercel

--- vite.config.js
+++ vite.config.js
@@ -2,14 +2,5 @@ import { defineConfig } from "vite";
 import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
-  // Tailwind eklentisini çalıştırır
   plugins: [tailwindcss()],
-
-  // GitHub Pages için en güvenli yol budur:
-  base: "./",
-
-  build: {
-    // Paketlenen dosyaların 'dist' yerine doğrudan ana dizine yakın durması için (opsiyonel)
-    outDir: "dist",
-  },
 });
